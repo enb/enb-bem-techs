a7132da376d082d5fee65d52fd36be665ba70282
andrewblond <andrewblond@yandex.com>
2014-07-27 22:04:52 +0400

    Added `deps-provider` alias for `provide-deps` tech

---
+++ techs/deps-provider.js
@@ -0,0 +1,20 @@
+/**
+ * deps-provider
+ * =============
+ *
+ * Технология переименована в `provide-deps`
+ */
+var inherit = require('inherit');
+
+module.exports = inherit(require('./provide-deps'), {
+    getName: function () {
+        return 'deps-provider';
+    },
+
+    build: function () {
+        var logger = this.node.getLogger();
+
+        logger.logTechIsDeprecated(this._target, this.getName(), 'enb-bem', 'provide-deps');
+        this.__base();
+    }
+});
