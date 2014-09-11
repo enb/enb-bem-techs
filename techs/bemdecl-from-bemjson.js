15e65e25177b55573eddc826f44bc49e234172ea
andrewblond <andrewblond@yandex.com>
2014-09-11 17:47:55 +0400

    Added `bemdecl-from-bemjson` alias for `bemjson-to-bemdecl` tech

---
+++ techs/bemdecl-from-bemjson.js
@@ -0,0 +1,20 @@
+/**
+ * bemdecl-from-bemjson
+ * ====================
+ *
+ * Технология переименована в `bemjson-to-bemdecl`
+ */
+var inherit = require('inherit');
+
+module.exports = inherit(require('./bemjson-to-bemdecl'), {
+    getName: function () {
+        return 'bemdecl-from-bemjson';
+    },
+
+    build: function () {
+        var logger = this.node.getLogger();
+
+        logger.logTechIsDeprecated(this._target, this.getName(), 'enb-bem', 'bemjson-to-bemdecl', 'enb-bem');
+        this.__base();
+    }
+});
